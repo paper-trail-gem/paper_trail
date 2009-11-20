ba0982e9de89be64dd2c885302b623f07cb6d521
Zachery Hostens <zacheryph@gmail.com>
2009-11-19 20:51:25 -0600

    update test code to work without being in a rails app

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,13 +1,27 @@
-require 'rubygems'
-require 'active_support'
-require 'active_support/test_case'
-require 'shoulda'
-
-ENV['RAILS_ENV'] = 'test'
-ENV['RAILS_ROOT'] ||= File.dirname(__FILE__) + '/../../../..'
-
 require 'test/unit'
-require File.expand_path(File.join(ENV['RAILS_ROOT'], 'config/environment.rb'))
+RAILS_ROOT = File.join(File.dirname(__FILE__), %w{.. .. .. ..})
+$:.unshift(File.join(File.dirname(__FILE__), %w{.. lib}))
+
+unless defined?(ActiveRecord)
+  if File.directory? RAILS_ROOT + 'config'
+    puts 'using config/boot.rb'
+    ENV['RAILS_ENV'] = 'test'
+    require File.join(RAILS_ROOT, 'config', 'boot.rb')
+  else
+    # simply use installed gems if available
+    puts 'using rubygems'
+    require 'rubygems'
+    gem 'actionpack'; gem 'activerecord'; gem 'activesupport'; gem 'rails'
+  end
+
+  %w(action_pack action_controller active_record active_support initializer).each {|f| require f}
+end
+require 'shoulda'
+require 'paper_trail'
+
+ActionController::Routing::Routes.draw do |map|
+  map.connect ':controller/:action/:id'
+end
 
 def connect_to_database
   config = YAML::load(IO.read(File.dirname(__FILE__) + '/database.yml'))
