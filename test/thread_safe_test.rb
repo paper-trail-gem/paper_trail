9332ff25c2b1b8e747f6053d7e033d739d3e31d4
Andy Stewart <boss@airbladesoftware.com>
2010-03-19 14:19:01 +0000

    Make PaperTrail threadsafe.

--- test/thread_safe_test.rb
+++ test/thread_safe_test.rb
@@ -1,34 +1,32 @@
-require File.dirname(__FILE__) + '/test_helper.rb'
+require 'test_helper'
 
 class TestController < ActionController::Base
-  
   def current_user
-    @current_user ||= ActiveSupport::SecureRandom.hex(32)
+    Thread.current.object_id
   end
-
 end
 
 class ThreadSafeTest < Test::Unit::TestCase
-  
-  should "is thread safe when dealing with audit logging" do
+  should "be thread safe" do
     blocked = true
-    
-    blocked_thread = Thread.new do 
+
+    slow_thread = Thread.new do
       controller = TestController.new
-      controller.send(:set_whodunnit)
+      controller.send :set_whodunnit
       begin
-        puts "sleep for .001 sec"
-        sleep(0.001)
+        sleep 0.001
       end while blocked
       PaperTrail.whodunnit
     end
 
-    fast_running_thread = Thread.new do 
+    fast_thread = Thread.new do
       controller = TestController.new
-      controller.send(:set_whodunnit)
+      controller.send :set_whodunnit
+      who = PaperTrail.whodunnit
       blocked = false
-      PaperTrail.whodunnit
+      who
     end
-    assert_not_equal blocked_thread.value, fast_running_thread.value
+
+    assert_not_equal slow_thread.value, fast_thread.value
   end
 end
