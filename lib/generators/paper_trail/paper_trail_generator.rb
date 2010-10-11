e93456247728ddd19cefaf89856d516deb5815dc
Andy Stewart <boss@airbladesoftware.com>
2010-10-11 17:44:21 +0100

    Whitespace.

--- lib/generators/paper_trail/paper_trail_generator.rb
+++ lib/generators/paper_trail/paper_trail_generator.rb
@@ -3,13 +3,11 @@ require 'rails/generators/migration'
 
 class PaperTrailGenerator < Rails::Generators::NamedBase
   include Rails::Generators::Migration
-  
-  desc "Generates (but does not run) a migration to add a versions table."
 
+  desc "Generates (but does not run) a migration to add a versions table."
   source_root File.expand_path('../templates', __FILE__)
-  
   argument :name, :type => :string, :default => "create_versions"
-  
+
   # Implement the required interface for Rails::Generators::Migration.
   # taken from http://github.com/rails/rails/blob/master/activerecord/lib/generators/active_record.rb
   def self.next_migration_number(dirname)
@@ -23,6 +21,4 @@ class PaperTrailGenerator < Rails::Generators::NamedBase
   def create_migration_file
     migration_template 'create_versions.rb', 'db/migrate/create_versions.rb'
   end
-
-
 end
