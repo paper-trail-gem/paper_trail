ac4714de1f6f1dabdc781920849641d3c7801920
Andy Stewart <boss@airbladesoftware.com>
2011-04-01 10:37:36 +0100

    Use Turn for prettier test output.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,12 @@ ENV["RAILS_ENV"] = "test"
 require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 require "rails/test_help"
 
+begin
+  require 'turn'
+rescue LoadError
+  # noop
+end
+
 #ActionMailer::Base.delivery_method = :test
 #ActionMailer::Base.perform_deliveries = true
 #ActionMailer::Base.default_url_options[:host] = "test.com"
