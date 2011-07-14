f094f6e89dd51d95402721647f7d825d40aea312
Andy Stewart <boss@airbladesoftware.com>
2011-07-14 10:32:06 +0100

    Simplify quotation marks.

--- test/unit/model_test.rb
+++ test/unit/model_test.rb
@@ -109,7 +109,7 @@ class HasPaperTrailModelTest < ActiveSupport::TestCase
           assert_equal ({'name' => ['Henry', 'Harry']}), @widget.versions.last.changeset
         end
 
-        should 'not have stored changes if object_changes column doesn\'t exist' do
+        should "not have stored changes if object_changes column doesn't exist" do
           remove_object_changes_column
           Version.reset_column_information
           assert_nil @widget.versions.last.changeset
