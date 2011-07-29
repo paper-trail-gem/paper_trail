c1a3bbbcc1f2ad1c1321e22ac102af338e0c89bb
Tom Derks <tderks@gmail.com>
2011-07-29 15:28:57 +0200

    Shortened index name for mysql use

--- lib/generators/paper_trail/templates/create_version_associations.rb
+++ lib/generators/paper_trail/templates/create_version_associations.rb
@@ -6,12 +6,12 @@ class CreateVersionAssociations < ActiveRecord::Migration
       t.integer   :foreign_key_id
     end
     add_index :version_associations, [:version_id]
-    add_index :version_associations, [:foreign_key_name,:foreign_key_id]
+    add_index :version_associations, [:foreign_key_name,:foreign_key_id], :name => 'index_on_foreign_key_name_and foreign_key_id'
   end
 
   def self.down
     remove_index :version_associations, [:version_id]
-    remove_index :version_associations, [:foreign_key_name,:foreign_key_id]
+    remove_index :version_associations, [:foreign_key_name,:foreign_key_id], :name => 'index_on_foreign_key_name_and foreign_key_id'
     drop_table :version_associations
   end
 end
