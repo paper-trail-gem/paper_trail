188ff45119e3aced50b9bcd7138ebc254393270e
Ben <beanie@benle.de>
2013-05-11 00:49:41 +0300

    Update has_paper_trail.rb
    use new lambda syntax for has_many

--- lib/paper_trail/has_paper_trail.rb
+++ lib/paper_trail/has_paper_trail.rb
@@ -58,9 +58,8 @@ module PaperTrail
         self.versions_association_name = options[:versions] || :versions
 
         has_many self.versions_association_name,
-                 :class_name => version_class_name,
-                 :as         => :item,
-                 :order      => "#{PaperTrail.timestamp_field} ASC, #{self.version_key} ASC"
+                 lambda { |_model| order("#{PaperTrail.timestamp_field} ASC", "#{_model.class.version_key} ASC") },
+                 :class_name => self.version_class_name, :as => :item
                  
         after_create  :record_create, :if => :save_version? if !options[:on] || options[:on].include?(:create)
         before_update :record_update, :if => :save_version? if !options[:on] || options[:on].include?(:update)
