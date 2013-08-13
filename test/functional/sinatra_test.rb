efe386d0b1fd097b5038d7624546a2b6c3c413ce
Ben Atkins <benatkins@fullbridge.com>
2013-08-13 18:02:46 -0400

    Fixing the Sinatra tests so they pass in Ruby18

--- test/functional/sinatra_test.rb
+++ test/functional/sinatra_test.rb
@@ -3,11 +3,11 @@ require 'sinatra/base'
 
 # --- Tests for modular `Sinatra::Base` style ----
 class BaseApp < Sinatra::Base
-  ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: File.expand_path('../../dummy/db/test.sqlite3', __FILE__))
+  ActiveRecord::Base.establish_connection(:adapter => 'sqlite3', :database => File.expand_path('../../dummy/db/test.sqlite3', __FILE__))
   register PaperTrail::Sinatra
 
   get '/test' do
-    w = Widget.create!(name: 'foo')
+    w = Widget.create!(:name => 'foo')
     'Hello'
   end
 
@@ -19,16 +19,16 @@ end
 class PaperTrailModularSinatraTest < ActiveSupport::TestCase
   include Rack::Test::Methods
 
+  def app
+    @app ||= BaseApp
+  end
+
   test 'baseline' do
     assert_nil Widget.first
     assert_nil Widget.create.versions.first.whodunnit
   end
 
   context "`PaperTrail::Sinatra` in a `Sinatra::Base` application" do
-
-    def app
-      @app ||= BaseApp
-    end
   
     should "sets the `user_for_paper_trail` from the `current_user` method" do
       get '/test'
@@ -44,15 +44,15 @@ end
 
 # --- Tests for non-modular `Sinatra::Application` style ----
 class Sinatra::Application
-  ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: File.expand_path('../../dummy/db/test.sqlite3', __FILE__))
+  ActiveRecord::Base.establish_connection(:adapter => 'sqlite3', :database => File.expand_path('../../dummy/db/test.sqlite3', __FILE__))
 
   get '/test' do
-    w = Widget.create!(name: 'foo')
+    w = Widget.create!(:name => 'foo')
     'Hello'
   end
 
   def current_user
-    'foobar'
+    'raboof'
   end
 end
 
@@ -80,9 +80,8 @@ class PaperTrailSinatraTest < ActiveSupport::TestCase
       widget = Widget.first
       assert_not_nil widget
       assert_equal 1, widget.versions.size
-      assert_equal 'foobar', widget.versions.first.whodunnit
+      assert_equal 'raboof', widget.versions.first.whodunnit
     end
 
   end
 end
-
