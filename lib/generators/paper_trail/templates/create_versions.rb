7f34d95ea7a0d5a09db25280d1c26cc77fa523e0
Elan Perach <elanperach@gmail.com>
2014-03-31 15:56:49 +0300

    added updated_at to versions table
    This is so Models which are associated with paper_trail and don't have timestamps won't fail when trying to update or create.

--- lib/generators/paper_trail/templates/create_versions.rb
+++ lib/generators/paper_trail/templates/create_versions.rb
@@ -7,6 +7,7 @@ class CreateVersions < ActiveRecord::Migration
       t.string   :whodunnit
       t.text     :object
       t.datetime :created_at
+      t.datetime :updated_at
     end
     add_index :versions, [:item_type, :item_id]
   end
