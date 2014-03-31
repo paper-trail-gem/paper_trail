e500146257bd1762d63a87823f5e09b198092d77
Elan Perach <elanperach@gmail.com>
2014-03-31 18:43:21 +0300

    explicit table_name

--- lib/paper_trail/version.rb
+++ lib/paper_trail/version.rb
@@ -2,6 +2,7 @@ require File.expand_path('../version_concern', __FILE__)
 
 module PaperTrail
   class Version < ::ActiveRecord::Base
+    self.table_name = "paper_trail_versions"
     include PaperTrail::VersionConcern
   end
 end
