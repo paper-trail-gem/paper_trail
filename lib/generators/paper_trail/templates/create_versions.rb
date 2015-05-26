a6edeac9ccc7c7703a2d7c9f271df4744f8d4608
Stan Low <a22301613@yahoo.com.tw>
2015-05-27 00:14:01 +0800

    update template's hash syntax

--- lib/generators/paper_trail/templates/create_versions.rb
+++ lib/generators/paper_trail/templates/create_versions.rb
@@ -1,9 +1,9 @@
 class CreateVersions < ActiveRecord::Migration
   def change
     create_table :versions do |t|
-      t.string   :item_type, :null => false
-      t.integer  :item_id,   :null => false
-      t.string   :event,     :null => false
+      t.string   :item_type, null: false
+      t.integer  :item_id,   null: false
+      t.string   :event,     null: false
       t.string   :whodunnit
       t.text     :object
       t.datetime :created_at
