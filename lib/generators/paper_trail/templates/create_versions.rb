7fe1a7943758a6949f37f522d1399a5fc44b5124
Jared Beck <jared@jaredbeck.com>
2015-10-31 21:17:05 -0400

    Docs: Add warnings about time precision in MySQL
    [ci skip]

--- lib/generators/paper_trail/templates/create_versions.rb
+++ lib/generators/paper_trail/templates/create_versions.rb
@@ -13,6 +13,20 @@ class CreateVersions < ActiveRecord::Migration
       t.string   :event,     :null => false
       t.string   :whodunnit
       t.text     :object,    :limit => TEXT_BYTES
+
+      # Known issue in MySQL: fractional second precision
+      # -------------------------------------------------
+      #
+      # MySQL timestamp columns do not support fractional seconds unless
+      # defined with "fractional seconds precision". MySQL users should manually
+      # add fractional seconds precision to this migration, specifically, to
+      # the `created_at` column.
+      # (https://dev.mysql.com/doc/refman/5.6/en/fractional-seconds.html)
+      #
+      # MySQL users should also upgrade to rails 4.2, which is the first
+      # version of ActiveRecord with support for fractional seconds in MySQL.
+      # (https://github.com/rails/rails/pull/14359)
+      #
       t.datetime :created_at
     end
     add_index :versions, [:item_type, :item_id]
