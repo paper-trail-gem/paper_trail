a87dbfad0be60e0898b8a8365b302afb06713744
Jared Beck <jared@jaredbeck.com>
2015-11-02 00:15:23 -0500

    Extract class: RecordHistory
    The methods `#index`, `.subsequent`, and `.preceding` contain
    duplicated logic re: how to order, chronologically, a set of
    versions, specifically a set representing the history of a single
    record.
    
    The extracted class only supports `#index` so far, but it should
    be possilbe to use in `.subsequent` and `.preceding` also.

---
+++ lib/paper_trail/record_history.rb
@@ -0,0 +1,59 @@
+module PaperTrail
+
+  # Represents the history of a single record.
+  # @api private
+  class RecordHistory
+
+    # @param versions - ActiveRecord::Relation - All versions of the record.
+    # @param version_class - Class - Usually PaperTrail::Version,
+    #   but it could also be a custom version class.
+    # @api private
+    def initialize(versions, version_class)
+      @versions = versions
+      @version_class = version_class
+    end
+
+    # Returns ordinal position of `version` in `sequence`.
+    # @api private
+    def index(version)
+      sequence.index(version)
+    end
+
+    private
+
+    # Returns `@versions` in chronological order.
+    # @api private
+    def sequence
+      if @version_class.primary_key_is_int?
+        @versions.select(primary_key).order(primary_key.asc)
+      else
+        @versions.
+          select([timestamp, primary_key]).
+          order(@version_class.timestamp_sort_order)
+      end
+    end
+
+    # @return - Arel::Attribute - Attribute representing the primary key
+    #   of the version table. The column's data type is usually a serial
+    #   integer (the rails convention) but not always.
+    # @api private
+    def primary_key
+      table[@version_class.primary_key]
+    end
+
+    # @return - Arel::Table - The version table, usually named `versions`, but
+    #   not always.
+    # @api private
+    def table
+      @version_class.arel_table
+    end
+
+    # @return - Arel::Attribute - Attribute representing the timestamp column
+    #   of the version table, usually named `created_at` (the rails convention)
+    #   but not always.
+    # @api private
+    def timestamp
+      table[PaperTrail.timestamp_field]
+    end
+  end
+end
