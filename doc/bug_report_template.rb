c5bb890f45280ec476a3a900213d4c049887dedf
kirankumawat <kiran.kumawat@metacube.com>
2016-01-08 11:14:10 +0530

    Update bug_report_template.rb

--- doc/bug_report_template.rb
+++ doc/bug_report_template.rb
@@ -50,11 +50,49 @@ ActiveRecord::Schema.define do
   add_index :version_associations, [:version_id]
   add_index :version_associations, [:foreign_key_name, :foreign_key_id],
     name: 'index_version_associations_on_foreign_key'
+  # respect of #685 issue
+  create_table :supply_chain_requirement_versions do |t|
+    	t.string   :item_type,      null: false
+	    t.integer  :item_id,        null: false
+	    t.string   :event,          null: false
+	    t.string   :whodunnit
+	    t.json     :object
+	    t.datetime :created_at
+	    t.integer  :transaction_id
+  end
+  create_table :supply_chain_versions, force: true do |t|
+	    t.string   :item_type,      null: false
+	    t.integer  :item_id,        null: false
+	    t.string   :event,          null: false
+	    t.string   :whodunnit
+	    t.json     :object
+	    t.datetime :created_at
+	    t.integer  :transaction_id
+  end
+  # end
 end
 
 class User < ActiveRecord::Base
   has_paper_trail
 end
+# respect of #685 issue
+class SupplyChainVersion < PaperTrail::Version
+  self.table_name = :supply_chain_versions
+end
+
+class SupplyChainRequirementVersion < PaperTrail::Version
+  self.table_name = :supply_chain_requirement_versions
+end
+
+class SupplyChainRequirement < ActiveRecord::Base
+  has_paper_trail :class_name => 'SupplyChainRequirementVersion'
+end
+
+class SupplyChain <  ActiveRecord::Base
+  has_paper_trail :class_name => 'SupplyChainVersion', :only => [:compliant_status,:compliant_description]
+  has_many :supply_chain_requirements, inverse_of: :supply_chain
+end
+# end
 
 class BugTest < ActiveSupport::TestCase
   def test_1
@@ -62,4 +100,11 @@ class BugTest < ActiveSupport::TestCase
       User.create(first_name: "Jane")
     }
   end
+  
+  # respect of #685 issue
+  def test_2
+    requirements = SupplyChain.find(4).versions.find(6).reify(has_many: true).supply_chain_requirements
+    assert_equal(1, requirements.length) # but returning length 2 
+  end
+  # end
 end
