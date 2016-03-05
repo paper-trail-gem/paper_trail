5485d1dfcc22dc10b94853b5fe79fcd995ec5fe2
Jared Beck <jared@jaredbeck.com>
2016-03-05 17:20:12 -0500

    Fix Style/Proc

--- test/dummy/app/models/article.rb
+++ test/dummy/app/models/article.rb
@@ -2,18 +2,18 @@ class Article < ActiveRecord::Base
   has_paper_trail(
     ignore: [
       :title, {
-        abstract: Proc.new { |obj|
+        abstract: proc { |obj|
           ["ignore abstract", "Other abstract"].include? obj.abstract
         }
       }
     ],
-    only: [:content, { abstract: Proc.new { |obj| obj.abstract.present? } }],
+    only: [:content, { abstract: proc { |obj| obj.abstract.present? } }],
     skip: [:file_upload],
     meta: {
       answer: 42,
       action: :action_data_provider_method,
-      question: Proc.new { "31 + 11 = #{31 + 11}" },
-      article_id: Proc.new { |article| article.id },
+      question: proc { "31 + 11 = #{31 + 11}" },
+      article_id: proc { |article| article.id },
       title: :title
     }
   )
