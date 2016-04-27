e36ba138a30464e613167840e72c8c147e3676fe
Jared Beck <jared@jaredbeck.com>
2016-04-26 22:06:22 -0400

    Do not track associations by default
    [Fixes #740]

--- spec/paper_trail/config_spec.rb
+++ spec/paper_trail/config_spec.rb
@@ -13,5 +13,21 @@ module PaperTrail
         expect { described_class.new }.to raise_error(NoMethodError)
       end
     end
+
+    describe "track_associations?" do
+      context "@track_associations is nil" do
+        after do
+          PaperTrail.config.track_associations = true
+        end
+
+        it "returns false and prints a deprecation warning" do
+          config = described_class.instance
+          config.track_associations = nil
+          expect {
+            expect(config.track_associations?).to eq(false)
+          }.to output(/DEPRECATION WARNING/).to_stderr
+        end
+      end
+    end
   end
 end
