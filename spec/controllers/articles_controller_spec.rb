62202a7487383ba69c177c6cf47e5b3309407cfc
Jared Beck <jared@jaredbeck.com>
2016-12-04 18:09:49 -0500

    Convert first controller test to rspec

---
+++ spec/controllers/articles_controller_spec.rb
@@ -0,0 +1,28 @@
+require "rails_helper"
+
+RSpec.describe ArticlesController, type: :controller do
+  describe "PaperTrail.enabled_for_controller?" do
+    context "PaperTrail.enabled? == true" do
+      before { PaperTrail.enabled = true }
+
+      it "returns true" do
+        assert PaperTrail.enabled?
+        post :create, params_wrapper(article: { title: "Doh", content: FFaker::Lorem.sentence })
+        expect(assigns(:article)).to_not be_nil
+        assert PaperTrail.enabled_for_controller?
+        assert_equal 1, assigns(:article).versions.length
+      end
+
+      after { PaperTrail.enabled = false }
+    end
+
+    context "PaperTrail.enabled? == false" do
+      it "returns false" do
+        assert !PaperTrail.enabled?
+        post :create, params_wrapper(article: { title: "Doh", content: FFaker::Lorem.sentence })
+        assert !PaperTrail.enabled_for_controller?
+        assert_equal 0, assigns(:article).versions.length
+      end
+    end
+  end
+end
