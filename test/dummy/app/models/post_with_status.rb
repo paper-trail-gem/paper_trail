8e6efc6052099e177392749b804d0ac31ea7fd58
Jared Beck <jared@jaredbeck.com>
2016-12-04 17:02:06 -0500

    Simplify test suite after dropping rails 3 support
    Also, the only version of rails 4 that we test against is 4.2, so
    we can drop the conditional around `ActiveRecord::Enum`.

--- test/dummy/app/models/post_with_status.rb
+++ test/dummy/app/models/post_with_status.rb
@@ -1,8 +1,7 @@
+# This model tests ActiveRecord::Enum, which was added in AR 4.1
+# http://edgeguides.rubyonrails.org/4_1_release_notes.html#active-record-enums
 class PostWithStatus < ActiveRecord::Base
   has_paper_trail
 
-  # ActiveRecord::Enum is only supported in AR4.1+
-  if defined?(ActiveRecord::Enum)
-    enum status: { draft: 0, published: 1, archived: 2 }
-  end
+  enum status: { draft: 0, published: 1, archived: 2 }
 end
