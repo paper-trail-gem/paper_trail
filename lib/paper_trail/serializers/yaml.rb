6d7b93ff49238ddfe346b54d74a7ca508433ff3f
Jared Beck <jared@jaredbeck.com>
2017-03-31 22:26:41 -0400

    Remove unreachable code re: syck
    In ruby >= 2.1, the removed code was no longer reachable, because
    `YAML::ENGINE.yamler` is always psych, never syck.

--- lib/paper_trail/serializers/yaml.rb
+++ lib/paper_trail/serializers/yaml.rb
@@ -24,32 +24,9 @@ module PaperTrail
       # value in the serialized `object_changes`.
       def where_object_changes_condition(arel_field, field, value)
         # Need to check first (before) and secondary (after) fields
-        m1 = nil
-        m2 = nil
-        case yaml_engine_id
-        when :psych
-          m1 = "%\n#{field}:\n- #{value}\n%"
-          m2 = "%\n#{field}:\n-%\n- #{value}\n%"
-        when :syck
-          # Syck adds extra spaces into array dumps
-          m1 = "%\n#{field}: \n%- #{value}\n%"
-          m2 = "%\n#{field}: \n-%\n- #{value}\n%"
-        else
-          raise "Unknown yaml engine"
-        end
+        m1 = "%\n#{field}:\n- #{value}\n%"
+        m2 = "%\n#{field}:\n-%\n- #{value}\n%"
         arel_field.matches(m1).or(arel_field.matches(m2))
-      end
-
-      # Returns a symbol identifying the YAML engine. Syck was removed from
-      # the ruby stdlib in ruby 2.0, but is still available as a gem.
-      # @api private
-      def yaml_engine_id
-        if (defined?(::YAML::ENGINE) && ::YAML::ENGINE.yamler == "psych") ||
-            (defined?(::Psych) && ::YAML == ::Psych)
-          :psych
-        else
-          :syck
-        end
       end
     end
   end
