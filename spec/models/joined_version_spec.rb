cf99fb33589351bc2ff4393b33489704f6a9c631
Jared Beck <jared@jaredbeck.com>
2017-04-01 20:13:08 -0400

    Lint: Finish fixing RSpec/NestedGroups

--- spec/models/joined_version_spec.rb
+++ spec/models/joined_version_spec.rb
@@ -1,47 +1,41 @@
 require "rails_helper"
 
-describe JoinedVersion, type: :model, versioning: true do
-  it { expect(JoinedVersion.superclass).to be PaperTrail::Version }
-
+RSpec.describe JoinedVersion, type: :model, versioning: true do
   let(:widget) { Widget.create!(name: FFaker::Name.name) }
   let(:version) { JoinedVersion.first }
 
-  describe "Scopes" do
-    describe "default_scope" do
-      it { expect(JoinedVersion.default_scopes).not_to be_empty }
+  describe "default_scope" do
+    it { expect(JoinedVersion.default_scopes).not_to be_empty }
+  end
+
+  describe "VersionConcern::ClassMethods" do
+    before { widget } # persist a widget
+
+    describe "#subsequent" do
+      it "does not raise error when there is a default_scope that joins" do
+        JoinedVersion.subsequent(version).first
+      end
     end
 
-    describe "VersionConcern::ClassMethods" do
-      before { widget } # persist a widget
-
-      describe "#subsequent" do
-        it "does not raise error when there is a default_scope that joins" do
-          JoinedVersion.subsequent(version).first
-        end
+    describe "#preceding" do
+      it "does not raise error when there is a default scope that joins" do
+        JoinedVersion.preceding(version).first
       end
+    end
 
-      describe "#preceding" do
-        it "does not raise error when there is a default scope that joins" do
-          JoinedVersion.preceding(version).first
-        end
-      end
-
-      describe "#between" do
-        it "does not raise error when there is a default scope that joins" do
-          JoinedVersion.between(Time.now, 1.minute.from_now).first
-        end
+    describe "#between" do
+      it "does not raise error when there is a default scope that joins" do
+        JoinedVersion.between(Time.now, 1.minute.from_now).first
       end
     end
   end
 
-  describe "Methods" do
-    describe "#index" do
-      it { is_expected.to respond_to(:index) }
+  describe "#index" do
+    it { is_expected.to respond_to(:index) }
 
-      it "does not raise error when there is a default scope that joins" do
-        widget # persist a widget
-        version.index
-      end
+    it "does not raise error when there is a default scope that joins" do
+      widget # persist a widget
+      version.index
     end
   end
 end
--- spec/models/skipper_spec.rb
+++ spec/models/skipper_spec.rb
@@ -1,45 +1,41 @@
 require "rails_helper"
 
-describe Skipper, type: :model do
-  with_versioning do
-    it { is_expected.to be_versioned }
+RSpec.describe Skipper, type: :model, versioning: true do
+  it { is_expected.to be_versioned }
 
-    describe "#update_attributes!", versioning: true do
-      context "updating a skipped attribute" do
-        let(:t1) { Time.zone.local(2015, 7, 15, 20, 34, 0) }
-        let(:t2) { Time.zone.local(2015, 7, 15, 20, 34, 30) }
+  describe "#update_attributes!", versioning: true do
+    context "updating a skipped attribute" do
+      let(:t1) { Time.zone.local(2015, 7, 15, 20, 34, 0) }
+      let(:t2) { Time.zone.local(2015, 7, 15, 20, 34, 30) }
 
-        it "does not create a version" do
-          skipper = Skipper.create!(another_timestamp: t1)
-          expect {
-            skipper.update_attributes!(another_timestamp: t2)
-          }.not_to(change { skipper.versions.length })
-        end
+      it "does not create a version" do
+        skipper = Skipper.create!(another_timestamp: t1)
+        expect {
+          skipper.update_attributes!(another_timestamp: t2)
+        }.not_to(change { skipper.versions.length })
+      end
+    end
+  end
+
+  describe "#reify" do
+    let(:t1) { Time.zone.local(2015, 7, 15, 20, 34, 0) }
+    let(:t2) { Time.zone.local(2015, 7, 15, 20, 34, 30) }
+
+    context "without preserve (default)" do
+      it "has no timestamp" do
+        skipper = Skipper.create!(another_timestamp: t1)
+        skipper.update_attributes!(another_timestamp: t2, name: "Foobar")
+        skipper = skipper.versions.last.reify
+        expect(skipper.another_timestamp).to be(nil)
       end
     end
 
-    describe "reify" do
-      context "reifying a with a skipped attribute" do
-        let(:t1) { Time.zone.local(2015, 7, 15, 20, 34, 0) }
-        let(:t2) { Time.zone.local(2015, 7, 15, 20, 34, 30) }
-
-        context "without preserve (default)" do
-          it "has no timestamp" do
-            skipper = Skipper.create!(another_timestamp: t1)
-            skipper.update_attributes!(another_timestamp: t2, name: "Foobar")
-            skipper = skipper.versions.last.reify
-            expect(skipper.another_timestamp).to be(nil)
-          end
-        end
-
-        context "with preserve" do
-          it "preserves its timestamp" do
-            skipper = Skipper.create!(another_timestamp: t1)
-            skipper.update_attributes!(another_timestamp: t2, name: "Foobar")
-            skipper = skipper.versions.last.reify(unversioned_attributes: :preserve)
-            expect(skipper.another_timestamp).to eq(t2)
-          end
-        end
+    context "with preserve" do
+      it "preserves its timestamp" do
+        skipper = Skipper.create!(another_timestamp: t1)
+        skipper.update_attributes!(another_timestamp: t2, name: "Foobar")
+        skipper = skipper.versions.last.reify(unversioned_attributes: :preserve)
+        expect(skipper.another_timestamp).to eq(t2)
       end
     end
   end
