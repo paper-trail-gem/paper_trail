5c1c54354389c7ea0ba8ddd2ea461cffa2350afd
chimame <rito.tamata@gmail.com>
2017-09-20 21:38:45 +0900

    Fix get version data with full name of class

--- lib/paper_trail/reifiers/has_many.rb
+++ lib/paper_trail/reifiers/has_many.rb
@@ -98,7 +98,7 @@ module PaperTrail
             select("MIN(version_id)").
             where("foreign_key_name = ?", assoc.foreign_key).
             where("foreign_key_id = ?", model.id).
-            where("#{version_table}.item_type = ?", assoc.class_name).
+            where("#{version_table}.item_type = ?", assoc.klass.name).
             where("created_at >= ? OR transaction_id = ?", version_at, tx_id).
             group("item_id").
             to_sql
