39bda99b1d42aca2c11265138a35cf2ea1d860a7
Hubert Pompecki <hpompecki@gmail.com>
2017-12-04 16:05:13 +0000

    Add comments

--- lib/paper_trail/attribute_serializers/attribute_serializer.rb
+++ lib/paper_trail/attribute_serializers/attribute_serializer.rb
@@ -2,7 +2,10 @@ require "paper_trail/attribute_serializers/array_serializer"
 
 module PaperTrail
   module AttributeSerializers
-    # :nodoc:
+    # Values returned by some Active Record serializers are
+    # not suited for JSON encoding. This factory
+    # replaces certain default Active Record serializers
+    # with custom PaperTrail ones.
     module AttributeSerializer
       def self.for(klass, attr)
         case active_record_serializer = klass.type_for_attribute(attr)
