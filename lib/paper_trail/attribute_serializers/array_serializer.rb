dd7f2392cb398dbcf885e11716d2ccb3987957b2
Hubert Pompecki <hpompecki@gmail.com>
2017-12-04 17:01:29 +0000

    Fix specs for postgres and ar < 5.0.2

--- lib/paper_trail/attribute_serializers/array_serializer.rb
+++ lib/paper_trail/attribute_serializers/array_serializer.rb
@@ -8,14 +8,30 @@ module PaperTrail
       end
 
       def serialize(array)
+        return serialize_with_ar(array) if active_record_pre_502?
         array
       end
 
       def deserialize(array)
+        return deserialize_with_ar(array) if active_record_pre_502?
+
         case array
         when ::String then deserialize_with_ar(array)
         else array
         end
+      end
+
+      private
+
+      def active_record_pre_502?
+        ::ActiveRecord::VERSION::MAJOR < 5 ||
+          (::ActiveRecord::VERSION::MINOR.zero? && ::ActiveRecord::VERSION::TINY < 2)
+      end
+
+      def serialize_with_ar(array)
+        ActiveRecord::ConnectionAdapters::PostgreSQL::OID::Array.
+          new(@subtype, @delimiter).
+          serialize(array)
       end
 
       def deserialize_with_ar(array)
