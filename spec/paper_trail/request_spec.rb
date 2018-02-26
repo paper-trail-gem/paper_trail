04a58ee2462c7a07eeef65127fc2158faf06846e
Alex Evanczuk <alex.evanczuk@gusto.com>
2018-02-25 18:40:46 -0800

    Add specs to request variables branch (#1052)

--- spec/paper_trail/request_spec.rb
+++ spec/paper_trail/request_spec.rb
@@ -22,6 +22,55 @@ module PaperTrail
       end
     end
 
+    describe ".enabled_for_model" do
+      it "sets enabled_for_model? to true" do
+        PaperTrail.request.enabled_for_model(Widget, false)
+        expect(PaperTrail.request.enabled_for_model?(Widget)).to eq(false)
+        PaperTrail.request.enabled_for_model(Widget, true)
+        expect(PaperTrail.request.enabled_for_model?(Widget)).to eq(true)
+      end
+
+      after do
+        PaperTrail.request.enable_model(Widget)
+      end
+    end
+
+    describe ".enabled_for_controller?" do
+      it "returns true" do
+        expect(PaperTrail.request.enabled_for_controller?).to eq(true)
+      end
+    end
+
+    describe ".enabled_for_controller=" do
+      it "sets enabled_for_controller? to true" do
+        PaperTrail.request.enabled_for_controller = true
+        expect(PaperTrail.request.enabled_for_controller?).to eq(true)
+        PaperTrail.request.enabled_for_controller = false
+        expect(PaperTrail.request.enabled_for_controller?).to eq(false)
+      end
+
+      after do
+        PaperTrail.request.enabled_for_controller = true
+      end
+    end
+
+    describe ".controller_info" do
+      it "returns an empty hash" do
+        expect(PaperTrail.request.controller_info).to eq({})
+      end
+    end
+
+    describe ".controller_info=" do
+      it "sets controller_info" do
+        PaperTrail.request.controller_info = { foo: :bar }
+        expect(PaperTrail.request.controller_info).to eq(foo: :bar)
+      end
+
+      after do
+        PaperTrail.request.controller_info = {}
+      end
+    end
+
     describe ".enable_model" do
       it "sets enabled_for_model? to true" do
         PaperTrail.request.disable_model(Widget)
@@ -44,23 +93,85 @@ module PaperTrail
           expect(described_class.whodunnit).to eq(2)
         end
       end
+
+      context "when set to a primtive value" do
+        it "returns the primitive value" do
+          described_class.whodunnit = :some_whodunnit
+          expect(described_class.whodunnit).to eq(:some_whodunnit)
+        end
+      end
     end
 
     describe ".with" do
       context "block given" do
-        it "sets whodunnit only for the block passed" do
-          described_class.with(whodunnit: "foo") do
-            expect(described_class.whodunnit).to eq("foo")
+        context "all allowed options" do
+          it "sets options only for the block passed" do
+            described_class.whodunnit = "some_whodunnit"
+            described_class.enabled_for_model(Widget, true)
+
+            described_class.with(whodunnit: "foo", enabled_for_Widget: false) do
+              expect(described_class.whodunnit).to eq("foo")
+              expect(described_class.enabled_for_model?(Widget)).to eq false
+            end
+            expect(described_class.whodunnit).to eq "some_whodunnit"
+            expect(described_class.enabled_for_model?(Widget)).to eq true
           end
-          expect(described_class.whodunnit).to be_nil
+
+          it "sets options only for the current thread" do
+            described_class.whodunnit = "some_whodunnit"
+            described_class.enabled_for_model(Widget, true)
+
+            described_class.with(whodunnit: "foo", enabled_for_Widget: false) do
+              expect(described_class.whodunnit).to eq("foo")
+              expect(described_class.enabled_for_model?(Widget)).to eq false
+              Thread.new { expect(described_class.whodunnit).to be_nil }.join
+              Thread.new { expect(described_class.enabled_for_model?(Widget)).to eq true }.join
+            end
+            expect(described_class.whodunnit).to eq "some_whodunnit"
+            expect(described_class.enabled_for_model?(Widget)).to eq true
+          end
         end
 
-        it "sets whodunnit only for the current thread" do
-          described_class.with(whodunnit: "foo") do
-            expect(described_class.whodunnit).to eq("foo")
-            Thread.new { expect(described_class.whodunnit).to be_nil }.join
+        context "some invalid options" do
+          it "raises an invalid option error" do
+            subject = proc do
+              described_class.with(whodunnit: "blah", invalid_option: "foo") do
+                raise "This block should not be reached"
+              end
+            end
+
+            expect { subject.call }.to raise_error(PaperTrail::Request::InvalidOption) do |e|
+              expect(e.message).to eq "Invalid option: invalid_option"
+            end
           end
-          expect(described_class.whodunnit).to be_nil
+        end
+
+        context "all invalid options" do
+          it "raises an invalid option error" do
+            subject = proc do
+              described_class.with(invalid_option: "foo", other_invalid_option: "blah") do
+                raise "This block should not be reached"
+              end
+            end
+
+            expect { subject.call }.to raise_error(PaperTrail::Request::InvalidOption) do |e|
+              expect(e.message).to eq "Invalid option: invalid_option"
+            end
+          end
+        end
+
+        context "private options" do
+          it "raises an invalid option error" do
+            subject = proc do
+              described_class.with(transaction_id: "blah") do
+                raise "This block should not be reached"
+              end
+            end
+
+            expect { subject.call }.to raise_error(PaperTrail::Request::InvalidOption) do |e|
+              expect(e.message).to eq "Cannot set private option: transaction_id"
+            end
+          end
         end
       end
     end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,8 @@ unless File.exist?(File.expand_path("dummy_app/config/database.yml", __dir__))
 end
 
 RSpec.configure do |config|
+  config.example_status_persistence_file_path = ".rspec_results"
+
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
