8a79be392cf65f6ae4e5142d94ae8251e6b4b5f2
Alex Evanczuk <alex.evanczuk@gusto.com>
2018-02-25 18:40:46 -0800

    Add specs to request variables branch (#1052)

--- spec/paper_trail_spec.rb
+++ spec/paper_trail_spec.rb
@@ -98,4 +98,51 @@ RSpec.describe PaperTrail do
       expect(described_class.version).to eq(described_class::VERSION::STRING)
     end
   end
+
+  describe "deprecated methods" do
+    before do
+      allow(ActiveSupport::Deprecation).to receive(:warn)
+    end
+
+    shared_examples "it delegates to request" do |method, args|
+      it do
+        arguments = args || [no_args]
+        allow(described_class.request).to receive(method)
+        described_class.public_send(method, *args)
+        expect(described_class.request).to have_received(method).with(*arguments)
+        expect(ActiveSupport::Deprecation).to have_received(:warn)
+      end
+    end
+
+    it_behaves_like "it delegates to request", :clear_transaction_id, nil
+    it_behaves_like "it delegates to request", :enabled_for_controller=, [true]
+    it_behaves_like "it delegates to request", :enabled_for_model, [Widget, true]
+    it_behaves_like "it delegates to request", :enabled_for_model?, [Widget]
+    it_behaves_like "it delegates to request", :whodunnit=, [:some_whodunnit]
+    it_behaves_like "it delegates to request", :whodunnit, nil
+    it_behaves_like "it delegates to request", :controller_info=, [:some_whodunnit]
+    it_behaves_like "it delegates to request", :controller_info, nil
+    it_behaves_like "it delegates to request", :transaction_id=, 123
+    it_behaves_like "it delegates to request", :transaction_id, nil
+
+    describe "whodunnit with block" do
+      it "delegates to request" do
+        allow(described_class.request).to receive(:with)
+        described_class.whodunnit(:some_whodunnit) { :some_block }
+        expect(ActiveSupport::Deprecation).to have_received(:warn)
+        expect(described_class.request).to have_received(:with) do |*args, &block|
+          expect(args).to eq([{ whodunnit: :some_whodunnit }])
+          expect(block.call).to eq :some_block
+        end
+      end
+    end
+
+    describe "whodunnit with invalid arguments" do
+      it "raises an error" do
+        expect { described_class.whodunnit(:some_whodunnit) }.to raise_error(ArgumentError) do |e|
+          expect(e.message).to eq "Invalid arguments"
+        end
+      end
+    end
+  end
 end
