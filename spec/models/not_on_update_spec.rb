97a776e255accafed316bb54a463fc19235002b8
Jared Beck <jared@jaredbeck.com>
2018-04-22 23:29:01 -0400

    Add save_with_version
    Replaces deprecated touch_with_version

--- spec/models/not_on_update_spec.rb
+++ spec/models/not_on_update_spec.rb
@@ -3,6 +3,16 @@
 require "spec_helper"
 
 RSpec.describe NotOnUpdate, type: :model do
+  describe "#save_with_version", versioning: true do
+    let!(:record) { described_class.create! }
+
+    it "creates a version, regardless" do
+      expect { record.paper_trail.save_with_version }.to change {
+        PaperTrail::Version.count
+      }.by(+1)
+    end
+  end
+
   describe "#touch_with_version", versioning: true do
     let!(:record) { described_class.create! }
 
