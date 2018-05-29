4d6f9bd0bebbfc315d56c651265a67577729d995
Jared Beck <jared@jaredbeck.com>
2018-05-29 18:26:19 -0400

    Delete spec callback "verify_stubs: false" ..
    .. we do not use it

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,29 +6,17 @@ ENV["DB"] ||= "sqlite"
 require "byebug"
 
 unless File.exist?(File.expand_path("dummy_app/config/database.yml", __dir__))
-  warn "WARNING: No database.yml detected for the dummy app, please run `rake prepare` first"
+  warn "No database.yml detected for the dummy app, please run `rake prepare` first"
 end
 
 RSpec.configure do |config|
   config.example_status_persistence_file_path = ".rspec_results"
-
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
-
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
   end
-
-  # Support for disabling `verify_partial_doubles` on specific examples.
-  config.around(:each, verify_stubs: false) do |ex|
-    config.mock_with :rspec do |mocks|
-      mocks.verify_partial_doubles = false
-      ex.run
-      mocks.verify_partial_doubles = true
-    end
-  end
-
   config.filter_run :focus
   config.run_all_when_everything_filtered = true
   config.disable_monkey_patching!
