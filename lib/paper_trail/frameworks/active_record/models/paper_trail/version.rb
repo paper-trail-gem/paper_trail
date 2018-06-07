8c9d86c4c32b9df629e6296233a3fc5f3136f258
Jared Beck <jared@jaredbeck.com>
2018-06-07 12:06:08 -0400

    Docs: Clarify that VersionAssociation is in separate gem

--- lib/paper_trail/frameworks/active_record/models/paper_trail/version.rb
+++ lib/paper_trail/frameworks/active_record/models/paper_trail/version.rb
@@ -4,9 +4,12 @@ require "paper_trail/version_concern"
 
 module PaperTrail
   # This is the default ActiveRecord model provided by PaperTrail. Most simple
-  # applications will only use this and its partner, `VersionAssociation`, but
-  # it is possible to sub-class, extend, or even do without this model entirely.
-  # See the readme for details.
+  # applications will use this model as-is, but it is possible to sub-class,
+  # extend, or even do without this model entirely. See documentation section
+  # 6.a. Custom Version Classes.
+  #
+  # The paper_trail-association_tracking gem provides a related model,
+  # `VersionAssociation`.
   class Version < ::ActiveRecord::Base
     include PaperTrail::VersionConcern
   end
