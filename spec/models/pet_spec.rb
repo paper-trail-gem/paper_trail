5370f28ed0988341cfd9f05c6637a29fc85fefa0
Lorin Thwaits <lorint@gmail.com>
2018-06-17 19:34:22 +0100

    Removed a require pry

--- spec/models/pet_spec.rb
+++ spec/models/pet_spec.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require "spec_helper"
-require 'pry-byebug'
+
 RSpec.describe Pet, type: :model, versioning: true do
   it "baseline test setup" do
     expect(Pet.new).to be_versioned
