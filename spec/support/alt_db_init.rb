2e1642214a476f88e30cc7b6e7d97a432ca616af
Lorin Thwaits <lorint@gmail.com>
2018-08-26 16:18:15 +0100

    Simpler migrations for Foo and Bar test databases

--- spec/support/alt_db_init.rb
+++ spec/support/alt_db_init.rb
@@ -40,8 +40,7 @@ end
 Foo::Base.configurations = configs
 Foo::Base.establish_connection(:foo)
 ActiveRecord::Base.establish_connection(:foo)
-paper_trail_migrations_path = File.expand_path("#{db_directory}/migrate/", __FILE__)
-::PaperTrailSpecMigrator.new(paper_trail_migrations_path).migrate
+::PaperTrailSpecMigrator.new.migrate
 
 module Bar
   class Base < ActiveRecord::Base
@@ -60,4 +59,4 @@ end
 Bar::Base.configurations = configs
 Bar::Base.establish_connection(:bar)
 ActiveRecord::Base.establish_connection(:bar)
-::PaperTrailSpecMigrator.new(paper_trail_migrations_path).migrate
+::PaperTrailSpecMigrator.new.migrate
