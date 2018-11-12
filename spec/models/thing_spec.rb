150d547403a6d0448c5447ee088c0082701eb900
Tyler Rick <tyler@tylerrick.com>
2018-11-12 08:34:21 -0800

    Update examples of customizing versions association

--- spec/models/thing_spec.rb
+++ spec/models/thing_spec.rb
@@ -6,14 +6,14 @@ RSpec.describe Thing, type: :model do
   describe "#versions", versioning: true do
     let(:thing) { Thing.create! }
 
-    it "applies the extend option" do
-      expect(thing.versions.singleton_class).to be < PrefixVersionsInspectWithCount
-      expect(thing.versions.inspect).to start_with("1 versions:")
-    end
-
     it "applies the scope option" do
       expect(Thing.reflect_on_association(:versions).scope).to be_a Proc
       expect(thing.versions.to_sql).to end_with "ORDER BY id desc"
+    end
+
+    it "applies the extend option" do
+      expect(thing.versions.singleton_class).to be < PrefixVersionsInspectWithCount
+      expect(thing.versions.inspect).to start_with("1 versions:")
     end
   end
 end
