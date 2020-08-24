646d7e2c5269ca0a487c8abbc1787db1875bd490
Jared Beck <jared@jaredbeck.com>
2020-08-24 00:21:46 -0400

    Release 11.0.0

--- lib/paper_trail/version_number.rb
+++ lib/paper_trail/version_number.rb
@@ -7,9 +7,9 @@ module PaperTrail
   # because of this confusion, but it's not worth the breaking change.
   # People are encouraged to use `PaperTrail.gem_version` instead.
   module VERSION
-    MAJOR = 10
-    MINOR = 3
-    TINY = 1
+    MAJOR = 11
+    MINOR = 0
+    TINY = 0
 
     # Set PRE to nil unless it's a pre-release (beta, rc, etc.)
     PRE = nil
