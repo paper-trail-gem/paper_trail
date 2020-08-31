c04ba0944abbe4f6302c2d255e586e83232ab1ce
Chris Hall <christopher.k.hall@gmail.com>
2020-08-31 15:19:49 -0400

    rix rubocop issues

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -45,9 +45,9 @@ RSpec.configure do |config|
 end
 
 RSpec.configure do |config|
-  [:controller, :view, :request].each do |type|
-    config.include ::Rails::Controller::Testing::TestProcess, :type => type
-    config.include ::Rails::Controller::Testing::TemplateAssertions, :type => type
-    config.include ::Rails::Controller::Testing::Integration, :type => type
+  %i[controller view request].each do |type|
+    config.include ::Rails::Controller::Testing::TestProcess, type: type
+    config.include ::Rails::Controller::Testing::TemplateAssertions, type: type
+    config.include ::Rails::Controller::Testing::Integration, type: type
   end
 end
