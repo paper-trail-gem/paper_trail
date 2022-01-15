ec7ddf622690350e5af44c6899ded9624065aabd
dfurber <dfurber@truecar.com>
2022-01-15 17:34:41 -0500

    from class_methods do back to module ClassMethods

--- lib/paper_trail/version_concern.rb
+++ lib/paper_trail/version_concern.rb
@@ -36,7 +36,7 @@ module PaperTrail
     end
 
     # :nodoc:
-    class_methods do
+    module ClassMethods
       def with_item_keys(item_type, item_id)
         where item_type: item_type, item_id: item_id
       end
