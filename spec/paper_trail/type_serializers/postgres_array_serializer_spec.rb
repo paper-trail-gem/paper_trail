d20c2216e5700ccfd23294f3d6895bcbdf312262
dfurber <dfurber@truecar.com>
2022-01-15 18:04:19 -0500

    lint the spec for PostgresArraySerializer

--- spec/paper_trail/type_serializers/postgres_array_serializer_spec.rb
+++ spec/paper_trail/type_serializers/postgres_array_serializer_spec.rb
@@ -7,22 +7,23 @@ module PaperTrail
     ::RSpec.describe PostgresArraySerializer do
       let(:word_array) { [].fill(0, rand(4..8)) { ::FFaker::Lorem.word } }
       let(:word_array_as_string) { word_array.join("|") }
-      subject { described_class.new("foo", "bar") }
+
+      let(:the_thing) { described_class.new("foo", "bar") }
 
       describe ".deserialize" do
         it "deserializes array to Ruby" do
-          expect(subject.deserialize(word_array)).to eq(word_array)
+          expect(the_thing.deserialize(word_array)).to eq(word_array)
         end
 
         it "deserializes string to Ruby array" do
-          expect(subject).to receive(:deserialize_with_ar).and_return(word_array)
-          expect(subject.deserialize(word_array_as_string)).to eq(word_array)
+          expect(the_thing).to have_received(:deserialize_with_ar).and_return(word_array)
+          expect(the_thing.deserialize(word_array_as_string)).to eq(word_array)
         end
       end
 
       describe ".dump" do
         it "serializes Ruby to JSON" do
-          expect(subject.serialize(word_array)).to eq(word_array)
+          expect(the_thing.serialize(word_array)).to eq(word_array)
         end
       end
 
