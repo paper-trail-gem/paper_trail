a0358eaa3d982c71321b7dc56196010092ea815a
wonda-tea-coffee <lagrange.resolvent@gmail.com>
2022-04-27 13:58:30 +0900

    replace git.io link
    @see
    https://github.blog/changelog/2022-04-25-git-io-deprecation/

--- lib/paper_trail/serializers/yaml.rb
+++ lib/paper_trail/serializers/yaml.rb
@@ -14,8 +14,8 @@ module PaperTrail
 
       # @param object (Hash | HashWithIndifferentAccess) - Coming from
       # `recordable_object` `object` will be a plain `Hash`. However, due to
-      # recent [memory optimizations](https://git.io/fjeYv), when coming from
-      # `recordable_object_changes`, it will be a `HashWithIndifferentAccess`.
+      # recent [memory optimizations](https://github.com/paper-trail-gem/paper_trail/pull/1189),
+      # when coming from `recordable_object_changes`, it will be a `HashWithIndifferentAccess`.
       def dump(object)
         object = object.to_hash if object.is_a?(HashWithIndifferentAccess)
         ::YAML.dump object
